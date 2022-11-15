177949b1f0ab79d33e35206fcba1bb539b2a92ce
Atheer Alotaibi <atheerf.otaibi@gmail.com>
2022-11-15 18:40:05 +0300

    update hero section

--- components/Sections/Hero.vue
+++ components/Sections/Hero.vue
@@ -4,6 +4,7 @@
       <div class="row flex-md-row-reverse align-items-center g-4 g-md-5">
         <div class="col col-md-6 ">
           <img
+          class="herobg"
             style="z-index: -1; position: absolute"
             :style="
               this.$dir() == 'rtl'
@@ -13,54 +14,57 @@
             src="~/assets/img/herobackground.svg"
             alt=""
           />
-          <img
-            style="z-index: 1"
-            class="img-fluid"
-            :style="
-              this.$dir() == 'rtl'
-                ? 'margin-right: 0%;  -webkit-transform: scaleX(-1);'
-                : 'margin-left: 0%;'
-            "
-            src="~/assets/img/zahmahero.svg"
-            alt=""
-          />
+          <div>
+            <img
+              v-if="this.$dir() == 'rtl'"
+              style="z-index: 1 mt-"
+              class="img-fluid herobg "
+              :style="
+                this.$dir() == 'ltr '
+                  ? 'margin-right: 0%;  -webkit-transform: scaleX(-1);'
+                  : 'margin-left: 0%;'
+              "
+              src="~/assets/img/zahmaOLaAr.svg"
+              alt=""
+            />
 
+            <img
+              v-else
+              style="z-index: 1"
+              class="img-fluid herobg"
+              :style="
+                this.$dir() == 'rtl'
+                  ? 'margin-right: 0%;  -webkit-transform: scaleX(-1);'
+                  : 'margin-left: 0%;'
+              "
+              src="~/assets/img/zahmahero.svg"
+              alt=""
+            />
+          </div>
         </div>
 
-
         <div class="col-md-6">
-          <h1 class="fw-bold lh-1 mb-3">{{ $t("herotitle") }}</h1>
-          <p class="lead py-2 py-md-5 fs-3 fw-light">{{ $t("herodesc") }}</p>
+          <h1 class="fw-bold lh-1 mb-3 animate__animated animate__fadeInLeft ">{{ $t("herotitle") }}</h1>
+          <p class="lead py-2 py-md-5 fs-3 fw-light animate__animated animate__fadeInLeft">{{ $t("herodesc") }}</p>
           <ul class="list-inline">
-            <li class="list-inline-item">
+            <li class="list-inline-item ">
               <a
                 href="https://apps.apple.com/us/app/%D8%B2%D8%AD%D9%85%D8%A9-%D8%A3%D9%88-%D9%84%D8%A7-zahma-o-la/id654895985"
               >
-                <img src="~/assets/img/AppStore.svg" alt=""
+                <img src="~/assets/img/AppStore.svg" height="auto" alt="App Store"
               /></a>
             </li>
 
-            <li class="list-inline-item pt-2">
+            <li class="list-inline-item pt-2 ">
               <a
                 href="https://play.google.com/store/apps/details?id=com.zahmaola.zahma"
               >
-                <img src="~/assets/img/GooglePlay.svg" alt=""
+                <img src="~/assets/img/GooglePlay.svg" height="auto" alt="Google Store"
               /></a>
             </li>
           </ul>
         </div>
-        <hr
-          style="
-            background: linear-gradient(
-              90deg,
-              #38b980 0%,
-              rgba(235, 224, 104, 0.9) 31.25%,
-              rgba(208, 103, 65, 0.97) 74.48%,
-              #c73c34 100%
-            );
-          "
-          size="5px"
-        />
+        <hr class="myhrline" />
       </div>
     </div>
   </section>
--- pages/index.vue
+++ pages/index.vue
@@ -5,36 +5,13 @@
     <SectionsFeatures />
     <SectionsDownload />
     <SectionsContact />
+    
   </div>
 </template>
 
 <script>
 export default {
-  name: "IndexPage",
-  layout: "default",
-  head() {
-    return {
-      title: this.$t("title") + " - " + this.$t("description"),
-
-      //titleTemplate: null
-      meta: [
-        {
-          hid: "description",
-          name: "description",
-          content: this.$t("description"),
-        },
-        {
-          hid: "twitter:title",
-          name: "twitter:title",
-          content: this.$t("title"),
-        },
-        {
-          hid: "twitter:description",
-          name: "twitter:description",
-          content: this.$t("description"),
-        },
-      ],
-    };
-  },
-};
+  name: 'IndexPage',
+  layout: 'default',
+}
 </script>
