f04fb63819c742a287a7c8386130eb4672debf96
Mutlaq Aldhbuiub <matulkn@gmail.com>
2023-06-04 11:34:57 +0300

    Adding appeal page

---
+++ pages/appeal.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <div class="pipedriveWebForms"
+                    data-pd-webforms="https://webforms.pipedrive.com/f/72j49812vDrOQiypJxCdJON1j1owtxSOMM0N6MnbnhSgDpdvxUTB1o7u6ALGIxCuj1">
+                    <script src="https://webforms.pipedrive.com/f/loader"></script>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'IndexPage',
+        layout: 'default',
+    }
+</script>
